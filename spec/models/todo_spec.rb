daf25c5711171c7c6f3d95ef3ecbb45921656d6b
ReeganCodemaster <reeganviljoen@outlook.com>
2022-05-24 11:26:15 +0200

    creted todo spec

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
+require 'spec_helper'
+require 'action_controller'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #relationship tets
+  #Test if Todo has 1:m relationship
+  it {should have_many(:items).dependendent(:destroy)}
+  #validation test
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:created_by)}
 end
