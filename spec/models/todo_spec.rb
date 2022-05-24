24f2dff368b3d526e1dc8bf2cbc33b3b274c9edf
ReeganCodemaster <reeganviljoen@outlook.com>
2022-05-24 11:30:10 +0200

    fixed spelling mistake

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -5,7 +5,7 @@ require 'action_controller'
 RSpec.describe Todo, type: :model do
   #relationship tets
   #Test if Todo has 1:m relationship
-  it {should have_many(:items).dependendent(:destroy)}
+  it {should have_many(:items).dependent(:destroy)}
   #validation test
   it {should validate_presence_of(:title)}
   it {should validate_presence_of(:created_by)}
