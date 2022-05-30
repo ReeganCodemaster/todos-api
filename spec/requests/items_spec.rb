d02133a1c18b1550b5baea4950cc040420a05844
ReeganCodemaster <reeganviljoen@outlook.com>
2022-05-30 15:15:10 +0200

    creted item tests in spec file

---
+++ spec/requests/items_spec.rb
@@ -0,0 +1,117 @@
+require 'rails-helper'
+
+Rspec.describe 'Items API' do
+  let(:todo) { create(:todo) }
+  let(:items) { create(:item,20, todo_id: todo.id) }
+  let(:todo_id) {create(:todo_id: todo.id)}
+  let(:id) {create(:id: items.id)}
+
+  #tests for GET todos/:todo_id/items
+  describe 'GET /todos/:todo_id/items' do
+    before( get "/todos/#{todo_id}/items" )
+
+    context 'when todo exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all todo items' do
+        expect(json.size).to eq(20)
+      end
+    end
+
+    context 'when todo does not exist' do
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Todo/)
+      end
+    end
+  end
+
+  #tets for GET /todos/:todo_id/items
+  describe 'GET /todos/:todo_id/items/:id' do
+    before {get "/todos/#{todo_id}/items/#{id}"}
+
+    context 'when todo item exists' do
+      
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the item' do
+        expect(json['id']).to eq(id)
+      end
+    end
+    context 'when todo item does not exist' do
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Todo Item/)
+      end
+    end
+  end
+
+  describe 'POST /todos/:todo_id/items' do
+    let(:valid_attributes) eq({ name:'Visit Narnai', done:false })
+
+    context 'when request is valid' do
+      before {post "/todos/#{todo_id}/items", params:valid_attributes}
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when request is invalid' do
+      before {post "todos/#{todo_id}/items", params:{}}
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a failure message' do
+        expect(response.body).to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  #tets for PUT /todos/:todo_id/items/:id
+  describe 'PUT /todos/:todo_id/items/:id' do
+    let(:valid_attributes) eq({ name:'Mozart'})
+
+    context 'when todo item exists' do
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+
+      it 'updates the item' do 
+        expect(updates_item.name).to match(/Mozart/)
+      end
+    end
+
+    context 'when the todo item does not exist' do
+      let(:id) eq{ 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Todo Item/)
+      end
+    end
+  end
+  # tets for DELETE /todos/:todo_id/items/:id
+  describe 'DELETE /todos/:todo_id/items/:id' do
+    before {delete "/todos/#{todo_id}/items/#{id}"}
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
