8593117f009ba706514a476d1106b70251d34289
ReeganCodemaster <reeganviljoen@outlook.com>
2022-05-30 17:45:25 +0200

    aDDED TEST TO USER MODEL SPEC

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #association tets 
+  it {should have_many(:todos)}
+  #validation tests
+  it {should validate_presence_of(:name)}
+  it {should validate_presence_of(:email)}
+  it {should validate_presence_of(:password_digest)}
 end
