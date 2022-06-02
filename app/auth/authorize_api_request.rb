dfce6d90c279b82f85c3027e0b051c950a1b19bf
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-02 16:39:41 +0200

    fixed syntaax errors

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -15,20 +15,17 @@ class AuthorizeApiRequest
   def user
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
   rescue ActiveRecord::RecordNotFound => e
-    raise (
-      ExceptionHandler::InvalidToken,
-      ("#{Message.invalid_token} #{e.message}")
-    )
+    raise ExceptionHandler::InvalidToken,("#{Message.invalid_token} #{e.message}")
   end
 
   def decoded_auth_token
-    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+    @decoded_auth_token ||= JsonWebToken.decode http_auth_header
   end
 
   def http_auth_header
     if headers['Authorization'].present?
       return headers['Authorization'].split(' ').last
     end
-     raise(ExeptionHandler::MissingToken, Message.missing_token)
+     raise(ExceptionHandler::MissingToken, Message.missing_token)
   end
 end
