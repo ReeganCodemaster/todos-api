e8cf1d3f7e213cb68e9202dc3b3bb336f1aed39b
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-03 16:33:50 +0200

    fixed syntax error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.passsword).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response ,:created)
+  end
+
+  private
+  def usser_params
+    params.permit(
+      :name, 
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
 end
