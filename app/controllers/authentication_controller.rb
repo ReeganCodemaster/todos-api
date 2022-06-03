a9b6d029f62b6f3c46034c261e6a2f58fffa683f
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-03 14:57:36 +0200

    added authetication method and strong params to controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,3 +1,10 @@
 class AuthenticationController < ApplicationController
-  
+  def authenticate
+    auth_token = AuthenticateUser().new(auth_params[:email], auth_params[:password]).call
+    json_responses(auth_token: auth_token)
+  end
+  private
+  def auth_paramss
+    params.permit(:email, :password)
+  end
 end
