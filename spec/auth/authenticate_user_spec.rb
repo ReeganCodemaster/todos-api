8bb69aca6015a48bb6dc519c77eb07087f4621b5
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-06 14:51:53 +0200

    fixed spelling errors

--- spec/auth/authenticate_user_spec.rb
+++ spec/auth/authenticate_user_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe AuthenticateUser do
   subject(:invalid_auth_obj) { described_class.new('foo','bar') }
 
   describe '#call' do
-    context 'when valid credentails' do
+    context 'when valid credentials' do
       it 'returns an auth token' do
         token = valid_auth_obj.call
         expect(token).not_to be_nil
       end
     end
-     context 'when invalid credentails' do
+     context 'when invalid credentials' do
       it 'raises an authentication error' do
         expect {invalid_auth_obj.call }
           .to raise_error(
             ExceptionHandler::AuthenticationError,
-            /Invalid credentails/
+            /Invalid credentials/
           )
       end
      end
