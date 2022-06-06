93b062fd6f0bba394c5934c7ce5f74b44db5fecb
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-06 14:50:05 +0200

    fixed spelling and syntax errors

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,11 +2,12 @@ class AuthenticationController < ApplicationController
   skip_before_action :authorize_request, only: :authenticate
 
   def authenticate
-    auth_token = AuthenticateUser().new(auth_params[:email], auth_params[:password]).call
+    auth_token = 
+      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
     json_responses(auth_token: auth_token)
   end
   private
-  def auth_paramss
+  def auth_params
     params.permit(:email, :password)
   end
 end
