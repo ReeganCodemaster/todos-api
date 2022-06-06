554df7f8e21414c973d587a7447dd3347cf5eccd
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-06 09:51:29 +0200

    fixed syntax errors

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe AuthenticationController, type: :request do
   describe 'POST /auth/login' do
     let!(:user) { create(:user)}
-    let(:headers) {valid_helpers.except('Authorization') }
+    let(:headers) {valid_headers.except('Authorization') }
     let(:valid_credentials) do
       {
         email:user.email,
@@ -20,7 +20,7 @@ RSpec.describe AuthenticationController, type: :request do
     # before { allow(request).to recieve(:headers).and_retrun(:headers) }
 
     context 'when requesst is valid' do
-      before { post 'auth/login', params: valid_credentials, headers: headers }
+      before { post '/auth/login', params: valid_credentials, headers: headers }
 
       it 'retruns an authentication token' do
         expects(json['auth_token']).not_to be_nil
@@ -28,7 +28,7 @@ RSpec.describe AuthenticationController, type: :request do
     end
 
     context 'when request is invalid' do
-      before { post 'auth/login', params: invalid_credentials, headers: headers }
+      before { post '/auth/login', params: invalid_credentials, headers: headers }
 
       it 'retruns a failure message' do
         expects(json['message']).to match(/Invalid credentials/)
