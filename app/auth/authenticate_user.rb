ff2c83b4b8bb4436b74256210be8623b03c30d51
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-06 14:49:38 +0200

    fixed some syntax

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -5,15 +5,15 @@ class AuthenticateUser
   end
 
   def call
-    JsonWebToken.encode(user_id:user.id) if user
+    JsonWebToken.encode(user_id: user.id) if user
   end
 
   private
   attr_reader :email, :password
 
   def user
-    user = User.find_by(email:email)
+    user = User.find_by(email: email)
     return user if user && user.authenticate(password)
-    raise(ExceptionHandler::AuthenticationError, Message.invalid_credentails) 
+    raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials) 
   end
 end
