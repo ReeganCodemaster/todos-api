43ccff60691fccb5325e2142241476fb1108b370
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-07 10:59:05 +0200

    wrapped classes in version modules

--- app/controllers/v1/items_controller.rb
+++ app/controllers/v1/items_controller.rb
@@ -1,39 +1,41 @@
-class ItemsController < ApplicationController
-  before_action :set_todo
-  before_action :set_item, only: [:show, :update, :destroy]
+module V1
+  class ItemsController < ApplicationController
+    before_action :set_todo
+    before_action :set_item, only: [:show, :update, :destroy]
 
-  def index
-    json_response(@todo.items)
-  end
+    def index
+      json_response(@todo.items)
+    end
 
-  def show
-    json_response(@item)
-  end
+    def show
+      json_response(@item)
+    end
 
-  def create
-    @todo.items.create!(item_params)
-    json_response(@todo, :created)
-  end
+    def create
+      @todo.items.create!(item_params)
+      json_response(@todo, :created)
+    end
 
-  def update
-    @item.update(item_params)
-    head :no_content
-  end
-  
-  def destroy
-    @item.destroy
-    head :no_content
-  end
-  private
-  def set_todo
-    @todo = Todo.find(params[:todo_id])
-  end
+    def update
+      @item.update(item_params)
+      head :no_content
+    end
+    
+    def destroy
+      @item.destroy
+      head :no_content
+    end
+    private
+    def set_todo
+      @todo = Todo.find(params[:todo_id])
+    end
 
-  def set_item
-    @item = @todo.items.find_by!(id: params[:id]) if @todo
-  end
+    def set_item
+      @item = @todo.items.find_by!(id: params[:id]) if @todo
+    end
 
-  def item_params
-    params.permit(:name, :done)
+    def item_params
+      params.permit(:name, :done)
+    end
   end
 end
