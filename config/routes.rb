4175648da512876bffb5eb7486e417d3b949be60
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-07 11:18:21 +0200

    added v2 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope module: :v2 , constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+  
   scope module: :v1, constraints: ApiVersion.new('v1', true)do
     resources :todos do
       resources :items
