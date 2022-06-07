6a335eb5cd9730df60f168ebeb8a701d45bb124a
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-07 10:48:46 +0200

    added api versioning to roiutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :todos do
-    resources :items
+  scope module: :v1, constraints: ApiVersion.new('v1', true)do
+    resources :todos do
+      resources :items
+    end
   end
+  
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   # Defines the root path route ("/")
