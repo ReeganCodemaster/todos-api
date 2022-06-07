39a66f9ea67934972d1688895640c3db86aac2b4
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-07 10:07:25 +0200

    fixed spelling error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ skip_before_action :authorize_request, only: :create
 
   def create
     user = User.create!(user_params)
-    auth_token = AuthenticateUser.new(user.email, user.passsword).call
+    auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token }
     json_response(response,:created)
   end
